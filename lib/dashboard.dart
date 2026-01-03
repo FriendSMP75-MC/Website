6f71d15017a1dd37d4a67f35a20bacaec7fbe90c
Ashwath <ashwakausi@outlook.com>
2026-01-03 16:37:52 +0530

    fix: UUID Select able text

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -106,7 +106,7 @@ class _DashboardState extends State<Dashboard> {
                     Spacer(),
                     Padding(
                       padding: const EdgeInsets.all(8.0),
-                      child: Text(
+                      child: SelectableText(
                         'Auth ID: $authUUID',
                         textAlign: TextAlign.end,
                         ),
