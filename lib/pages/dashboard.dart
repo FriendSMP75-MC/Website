09279c34b5333922a55110ee6e332d933fa52f43
Ashwath <ashwakausi@outlook.com>
2026-01-18 15:43:48 +0530

    Test: Staff dashboard grid display fix -1

--- lib/pages/dashboard.dart
+++ lib/pages/dashboard.dart
@@ -341,7 +341,7 @@ class _DashboardState extends State<Dashboard> {
                   return Text("Error: ${snapshot.error}");
                 }
                 if (snapshot.data == true) {
-                  return Expanded(child: StaffDashboard());
+                  return SizedBox(height:MediaQuery.of(context).size.height,child:  StaffDashboard());
                 }
                 return Text('Hello there! \n unfortunately you don\'t have access to this page :(');
               },
