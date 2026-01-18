9243b7e00cf4f9e3544642ca81be3a7f7b06027f
Ashwath <ashwakausi@outlook.com>
2026-01-18 12:29:59 +0530

    Fix: staff dashboard

--- lib/pages/staff_dashboard.dart
+++ lib/pages/staff_dashboard.dart
@@ -11,7 +11,7 @@ class _StaffDashboardState extends State<StaffDashboard> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Staff Dashboard")),
+
       body: Column(
         children: [
           // Title
@@ -26,49 +26,47 @@ class _StaffDashboardState extends State<StaffDashboard> {
           // GridView fills remaining space
           SizedBox(
             height: double.infinity,
-            child: Expanded(
-              child: GridView.count(
-                crossAxisCount: 3,
-                crossAxisSpacing: 50,
-                mainAxisSpacing: 8,
-                padding: const EdgeInsets.all(8),
-                children: [
-                  Container(
-                    color: Colors.grey,
-                    child: Row(
-                      children: [
-                        Text(
-                          'Announcement',
-                          style: TextStyle(fontSize: 20),
-                          textAlign: TextAlign.left,
-                        ),
-                        SizedBox(height: 2,),
-                        ElevatedButton(onPressed: () {}, child: Text('hi'))
-                      ],
-                    ),
+            child: GridView.count(
+              crossAxisCount: 3,
+              crossAxisSpacing: 50,
+              mainAxisSpacing: 8,
+              padding: const EdgeInsets.all(8),
+              children: [
+                Container(
+                  color: Colors.grey,
+                  child: Row(
+                    children: [
+                      Text(
+                        'Announcement',
+                        style: TextStyle(fontSize: 20),
+                        textAlign: TextAlign.left,
+                      ),
+                      SizedBox(height: 2,),
+                      ElevatedButton(onPressed: () {}, child: Text('hi'))
+                    ],
                   ),
-                  Container(
-                    color: Colors.green,
-                    child: Center(child: Text('Gallery')),
-                  ),
-                  Container(
-                    color: Colors.blue,
-                    child: Center(child: Text('hello3')),
-                  ),
-                  Container(
-                    color: Colors.orange,
-                    child: Center(child: Text('Reports')),
-                  ),
-                  Container(
-                    color: Colors.purple,
-                    child: Center(child: Text('Settings')),
-                  ),
-                  Container(
-                    color: Colors.teal,
-                    child: Center(child: Text('Logout')),
-                  ),
-                ],
-              ),
+                ),
+                Container(
+                  color: Colors.green,
+                  child: Center(child: Text('Gallery')),
+                ),
+                Container(
+                  color: Colors.blue,
+                  child: Center(child: Text('hello3')),
+                ),
+                Container(
+                  color: Colors.orange,
+                  child: Center(child: Text('Reports')),
+                ),
+                Container(
+                  color: Colors.purple,
+                  child: Center(child: Text('Settings')),
+                ),
+                Container(
+                  color: Colors.teal,
+                  child: Center(child: Text('Logout')),
+                ),
+              ],
             ),
           ),
         ],
