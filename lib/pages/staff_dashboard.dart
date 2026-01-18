a687657467f470fc6b3c7443f143026a8169fc76
Ashwath <ashwakausi@outlook.com>
2026-01-18 12:15:51 +0530

    Add: Announcement card - test

--- lib/pages/staff_dashboard.dart
+++ lib/pages/staff_dashboard.dart
@@ -10,58 +10,66 @@ class StaffDashboard extends StatefulWidget {
 class _StaffDashboardState extends State<StaffDashboard> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        // Title
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Text(
-            'Staff Dashboard',
-            style: TextStyle(fontSize: 20),
+    return Scaffold(
+      appBar: AppBar(title: const Text("Staff Dashboard")),
+      body: Column(
+        children: [
+          // Title
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              'Staff Dashboard',
+              style: TextStyle(fontSize: 20),
+            ),
           ),
-        ),
 
-        SizedBox(
-          height: 200, 
-          child: GridView.count(
-            crossAxisCount: 3,
-            crossAxisSpacing: 50,
-            mainAxisSpacing: 8,
-            padding: const EdgeInsets.all(8),
-            children: [
-              Container(
-                color: Colors.blueGrey,
-                child: Column(
-                  children: [
-                    Text('Announcement',style: TextStyle(fontSize: 20),textAlign: TextAlign.left,),
-                    ElevatedButton(onPressed: (){}, child: Text('hi'))
-                  ],
-                )
-              ),
-              Container(
-                color: Colors.green,
-                child: Center(child: Text('hello2')),
-              ),
-              Container(
-                color: Colors.blue,
-                child: Center(child: Text('hello3')),
-              ),
-              Container(
-                color: Colors.orange,
-                child: Center(child: Text('Reports')),
-              ),
-              Container(
-                color: Colors.purple,
-                child: Center(child: Text('Settings')),
-              ),
-              Container(
-                color: Colors.teal,
-                child: Center(child: Text('Logout')),
-              ),
-            ],
+          // GridView fills remaining space
+          Expanded(
+            child: GridView.count(
+              crossAxisCount: 3,
+              crossAxisSpacing: 50,
+              mainAxisSpacing: 8,
+              padding: const EdgeInsets.all(8),
+              children: [
+                Container(
+                  color: Colors.grey,
+                  child: Row(
+                    children: [
+                      Text(
+                        'Announcement',
+                        style: TextStyle(fontSize: 20),
+                        textAlign: TextAlign.left,
+                      ),
+                      SizedBox(height: 2,),
+                      ElevatedButton(onPressed: () {}, child: Text('hi'))
+                    ],
+                  ),
+                ),
+                Container(
+                  color: Colors.green,
+                  child: Center(child: Text('Gallery')),
+                ),
+                Container(
+                  color: Colors.blue,
+                  child: Center(child: Text('hello3')),
+                ),
+                Container(
+                  color: Colors.orange,
+                  child: Center(child: Text('Reports')),
+                ),
+                Container(
+                  color: Colors.purple,
+                  child: Center(child: Text('Settings')),
+                ),
+                Container(
+                  color: Colors.teal,
+                  child: Center(child: Text('Logout')),
+                ),
+              ],
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
