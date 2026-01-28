c4f5efdfa2dea331f54a432da1637ad427482e24
Ashwath <ashwakausi@outlook.com>
2026-01-28 19:51:25 +0530

    Add: Support us page

--- lib/pages/support_us.dart
+++ lib/pages/support_us.dart
@@ -1,19 +1,41 @@
 import 'package:flutter/material.dart';
-import 'package:server_site/widgets/ad.dart';
+import 'package:web/web.dart' as web;
+import 'dart:js_interop'; // <--- Add this for .toJS extension
 
 class SupportUsPage extends StatelessWidget {
   const SupportUsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      loadAd();
-    });
-
     return Scaffold(
       appBar: AppBar(title: const Text("Support Us")),
-      body: const Center(
-        child: Text("Thanks for supporting us!"),
+      body: Column(
+        children: [
+          const Expanded(child: Center(child: Text("Thank you for your support!"))),
+          
+          SizedBox(
+            height: 100,
+            child: HtmlElementView.fromTagName(
+              tagName: 'div',
+              onElementCreated: (Object element) {
+                final web.HTMLElement htmlElement = element as web.HTMLElement;
+                
+                // Add .toJS at the end of the string to fix the error
+                htmlElement.innerHTML = '''
+                  <ins class="adsbygoogle"
+                       style="display:block"
+                       data-ad-client="ca-pub-2317643702082291"
+                       data-ad-slot="3452575408"
+                       data-ad-format="auto"
+                       data-full-width-responsive="true"></ins>
+                  <script>
+                       (adsbygoogle = window.adsbygoogle || []).push({});
+                  </script>
+                '''.toJS; 
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/widgets/ad.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:js_interop';
-
-@JS('adsbygoogle')
-external JSArray<JSAny?> get _adsbygoogle;
-
-void loadAd() {
-  final jsObj = JSObject(); // represents {}
-  _adsbygoogle.add(jsObj);
-}
